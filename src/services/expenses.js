426b85c6c65a4be3358021877754b5c72c41c3e3
Edwin Semakula <edwinsemakula@tfl.gov.uk>
2023-01-11 18:25:05 +0000

    Added two expenses js files

---
+++ src/services/expenses.js
@@ -0,0 +1,14 @@
+// Responsible for making http requests e.g. getting/creating expenses.
+// Reaching out to the API
+export const GetExpenses = async (dispatch) => {
+    try{
+        // api calling
+        const expenses = [
+            { id: 1, description: 'Groceries', amount: 60.82},
+            { id: 2, description: 'Rent', amount: 500},
+            { id: 3, description: 'Bills', amount: 150},
+        ];
+    } catch {
+        console.log('Error')
+    }
+}
